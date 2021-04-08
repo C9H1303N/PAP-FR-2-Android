f287a4ee1fa906ccc5fbd7f25016018703cd475f
姚子健 <18373251@buaa.edu.cn>
2021-04-08 13:54:19 +0800

    改findpass

---
+++ app/src/main/java/com/example/pap_fr_2_android/findpass.java
@@ -0,0 +1,14 @@
+package com.example.pap_fr_2_android;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class findpass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_findpass);
+    }
+}
